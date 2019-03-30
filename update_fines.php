2c569b005e3d77d96ca1bebfc951b0078cd77ab6
John Michael Baluyos <jmbaluyos@gmail.com>
2019-03-30 16:38:23 +0800

    modified update fines

--- update_fines.php
+++ update_fines.php
@@ -4,15 +4,18 @@
 	if (isset($_GET['EDIT3'])) {
 		$id_number = $_GET['EDIT3'];
 		$update = true;
-		$record = mysqli_query($db, "SELECT fines.id_number,fines.status,fines.date_paid,fines.event_code,Sum(penalty) as amount FROM fines WHERE id_number='$id_number'");
+		$record = mysqli_query($db, "SELECT fines.id_number,fines.status,fines.date_paid,fines.event_code as absent,Sum(penalty) as amount FROM fines WHERE id_number='$id_number'");
 		
 	
 			$result = mysqli_fetch_array($record);
 			$id_number = $result["id_number"];
-			$event_code = $result['event_code'];
 			$amount = $result["amount"];
 			$status = $result["status"];
 			$date_paid = $result["date_paid"];
+
+		/*$query = "SELECT fines.event_code FROM fines,event WHERE fines.event_code=event.event_code AND id_number = '$id_number'";
+		$results = mysqli_query($db, $query); 
+		$count = mysqli_num_rows($results);*/
 		
 	}	
 ?>
@@ -99,31 +102,37 @@
 				    <div class="col-md-2">
 				      <h6>Penalty: </h6><input type="text" class="form-control" value="<?php echo "₱".$amount; ?>" name="penalty" readonly>
 				  	</div>
-				  	<!-- <div class="col-md-4">
-				      <div class="form-group">
-					    <label for="exampleFormControlSelect2">Event</label>
-					    <select multiple class="form-control" id="exampleFormControlSelect2" name="event_code">
-					      	<option value = ""><?php echo $event_code ?></option>
-							
-							
-					      
-					    </select>
-					  </div>
-				  	</div> -->
-				  </div>
-				  <div class = "form-row">
-				    <div class="col-md-3">
+				  	<div class="col-md-3">
 				      <h6>Status: </h6>
 					    	<select name="status" class="form-control">
 					    		<option selected>Select Status</option>
 					    		<option value="Paid">Paid</option>
 					    	</select>
 				    </div>
+				  </div>
+				  <br />
+				  <div class = "form-row">
 				    <div class="col-md-4">
 				    	<h6>Date: </h6><input type="date" class="form-control" name="date_paid" >
 				    </div>
-				  </div>
-				  <br />
+				 	<div class="col-md-8">
+				      <div class="form-group">		
+					  <h6><label for="exampleFormControlSelect2">Details:</label></h6>
+						  <select multiple class="form-control" id="exampleFormControlSelect2" name="event_code">
+							<?php 
+								$query = "SELECT fines.event_code,event.event_name,fines.penalty,event.date FROM fines,event WHERE fines.event_code=event.event_code AND id_number = '$id_number'";
+								$results = mysqli_query($db, $query); 
+								$count = mysqli_num_rows($results);
+								if($count = 1){
+									while ($row = mysqli_fetch_array($results)){
+							?>
+									<option value = "<?php echo $row['event_code'] ?>"><?php echo "(".$row['event_code'].")"."  ".$row['event_name']." "."("."₱".$row['penalty'].")"." "."(".$row['date'].")"; ?></option>
+								
+								<?php } 
+					  			}?>
+						  </select>
+					  </div>
+				  	</div>
 				  <div class="form-row">
 					  <div class="col-md-4">
 						<?php if ($update == true): ?>
